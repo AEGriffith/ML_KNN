53fdd5b62aed77973a623f33224ed70fa0d19247
amandagriffith <QWTpL^}Y#8[~>
2020-11-05 19:20:02 -0500

    plotted features

--- hw03.py
+++ hw03.py
@@ -5,12 +5,22 @@ Assignment: 03
 Purpose: Choose a dataset and create a K-NN Classifier
 Due: November 10, 2020
 """
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
 """
 Imports
 """
+import pandas as pd
 import processing as prep
 import plot_data as plot
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
+
 
 """
 Variables
@@ -20,21 +30,37 @@ seed_filename = 'files/seeds_dataset.txt'
 seed_data_range = [0, 1, 2, 3, 4, 5, 6]
 seed_label_range = [7]
 
+# Variables for KNN parameters
+knn_k = [1, 3, 5, 7, 9]
+knn_weights = ['uniform', 'distance']
+knn_algorithms = ['ball_tree', 'kd_tree', 'brute', 'auto']
+knn_metrics = ['euclidean', 'manhattan', 'chebyshev', 'minkowski']
+parameters = {'k': knn_k, 'weight': knn_weights, 'algorithm': knn_algorithms, 'metric': knn_metrics}
+
 """
-Data Import and Processing
+Data Import
 """
-# Import Data from seed data text file and store as numpy arrays
-seed_data, seed_labels = prep.data_import(seed_filename, seed_data_range, seed_label_range)
+# Import Data from seed data text file
+seeds_df, seed_data, seed_labels = prep.data_import(seed_filename, seed_data_range, seed_label_range)
 
-# MinMax Data Normalization
-mm_seed = prep.min_max_norm(seed_data)
 
-# Z-score Data Normalization
-z_seed = prep.z_score_norm(seed_data)
+"""
+Data Visualization
+"""
+# get different classes
+x1 = seeds_df[seeds_df[7] == 1]
+x2 = seeds_df[seeds_df[7] == 2]
+x3 = seeds_df[seeds_df[7] == 3]
 
-# Plot Normalizations
-# plot.hist(mm_seed[:,0], mm_seed[:,1], mm_seed[:,2], mm_seed[:,3], mm_seed[:,4], mm_seed[:,5],
-#              mm_seed[:, 6])
-# plot.hist(z_seed[:,0], z_seed[:,1], z_seed[:,2], z_seed[:,3], z_seed[:,4], z_seed[:,5],
-#              z_seed[:, 6], False)
+# Plot features
+fig, axes = plt.subplots(ncols=3, nrows=3)
+axes = axes.flatten()
+for i in seed_data_range:
+    sns.histplot(seeds_df, x=seeds_df[i], hue=seeds_df[7], multiple="stack", ax=axes[i], legend=False)
+plt.show()
+
+"""
+Data Visualization
+"""
+
 
